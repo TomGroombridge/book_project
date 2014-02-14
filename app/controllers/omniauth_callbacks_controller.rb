e82844c507454b356b27f0184e45b9b932bb92a6
larahy <larahy@gmail.com>
2014-02-14 14:26:36 +0000

    Revert user stuff

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,8 +2,9 @@ class OmniauthCallbacksController < ApplicationController
 
 	def twitter
 		user = User.twitter_auth request.env["omniauth.auth"]
+		sign_in :user, user
 		user.selection = Selection.find session[:selection_id]
-    sign_in_and_redirect(user) and return
+    redirect_to "/users/#{current_user.name}"
 	end
 	
 end
