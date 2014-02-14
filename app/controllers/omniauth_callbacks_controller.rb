c54e3d56839ddcc222f1aa23b09f38f3a41bb250
larahy <larahy@gmail.com>
2014-02-14 14:24:07 +0000

    More fix

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class OmniauthCallbacksController < ApplicationController
 	def twitter
 		user = User.twitter_auth request.env["omniauth.auth"]
 		user.selection = Selection.find session[:selection_id]
-    sign_in_and_redirect user
+    sign_in_and_redirect(user) and return
 	end
 	
 end
